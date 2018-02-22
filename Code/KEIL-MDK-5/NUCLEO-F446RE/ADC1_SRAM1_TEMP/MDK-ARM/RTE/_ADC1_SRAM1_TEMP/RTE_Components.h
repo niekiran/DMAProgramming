0ec0079a0a9cfa15da6120008e3d266301c3d193
Kiran Nayak <niekiran@gmail.com>
2018-02-22 11:00:23 +0530

    Added ADC to SRAM keil project

---
+++ Code/KEIL-MDK-5/NUCLEO-F446RE/ADC1_SRAM1_TEMP/MDK-ARM/RTE/_ADC1_SRAM1_TEMP/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ADC1_SRAM1_TEMP' 
+ * Target:  'ADC1_SRAM1_TEMP' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
